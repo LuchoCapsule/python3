67b004f7082c8601786263050bf072e0de6a695a
Luis Armando <lagonzalez@xms.cl>
2024-10-01 14:51:26 -0300

    It enables CORS for all routes in a flask app

--- my_fb_chat_bot/app.py
+++ my_fb_chat_bot/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
+from flask_cors import CORS
 import requests
 import json
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 PAGE_ACCESS_TOKEN = 'EAB0zOeDA58cBO0zbyIEZA2wsjnHSBCZBTjE57ZB2lI09C9ivJOkKG2TU9v2B7KvrLMsGnaVKDeBMRnfcwYzk6x41JVtQR4aZCCrGTVfKTqwfCpnZCUstgHJ6r4glOXOPEnrJKeLAYm77bEYRyt71J7mNYu8ZCgBmjuVwkA2byUjUIUjgkPveC2gY9A5bDezx81ywZDZD'
-#VERIFY_TOKEN = 'YOUR_VERIFY_TOKEN'
 VERIFY_TOKEN = 'my_verify_token_12345'
 
 @app.route('/', methods=['GET'])
@@ -24,7 +25,6 @@ def webhook():
                 if messaging_event.get('message'):
                     sender_id = messaging_event['sender']['id']
                     message_text = messaging_event['message']['text']
-
                     reply_message(sender_id, message_text)
     return 'OK', 200
 
@@ -40,4 +40,3 @@ def reply_message(sender_id, message_text):
 if __name__ == '__main__':
     #app.run(port=5000)
     app.run()
-
